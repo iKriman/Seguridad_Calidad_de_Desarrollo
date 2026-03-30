47c5c82c894839ea44dbb64d67fe9d6335abc774
Ignacio Kriman <63005850+iKriman@users.noreply.github.com>
2026-03-30 01:25:09 -0300

    Delete Exp1_S2_Grupo8 directory

--- Exp1_S2_Grupo8/create.sql
+++
@@ -1,3 +0,0 @@
-create database veterinaria_db;
-create user 'veterinaria'@'%' identified by 'admin123';
-grant all on veterinaria_db.* to 'veterinaria'@'%';
--- Exp1_S2_Grupo8/src/main/java/com/duoc/backend/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.duoc.backend;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PatientRepository extends CrudRepository<Patient, Integer> {
-    // Additional query methods can be added here if needed
-}
